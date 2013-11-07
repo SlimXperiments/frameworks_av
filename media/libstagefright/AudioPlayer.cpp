330f0a60514cd3c9637ac3f8eb188ae70cb89613
Krishnankutty Kolathappilly <kkolat@codeaurora.org>
2013-11-06 18:19:28 -0800

    libstagefright: Fix intermittent pause during tunnel A-V playback
    -Media time is used in onVideoEvent for A-V sync. In offload
     playback we only use realtime(time from DSP).
    -Media time should be same as real time. If media time is not
     updated with the real time value A-V sync issues are seen.
    -Update media time to real time value to avoid intermittent pause
     during playback.
    
    CRs-Fixed: 572060
    Change-Id: Ib1123b7554f3508230ca67cd20a68fa32c753057

--- media/libstagefright/AudioPlayer.cpp
+++ media/libstagefright/AudioPlayer.cpp
@@ -726,6 +726,7 @@ int64_t AudioPlayer::getRealTimeUs() {
         playPosition = getOutputPlayPositionUs_l();
         if(!mReachedEOS)
             mPositionTimeRealUs = playPosition;
+        mPositionTimeMediaUs = mPositionTimeRealUs;
         return mPositionTimeRealUs;
     }
 
@@ -801,6 +802,7 @@ int64_t AudioPlayer::getMediaTimeUs() {
             mPositionTimeRealUs = playPosition;
         ALOGV("getMediaTimeUs getOutputPlayPositionUs_l() playPosition = %lld,\
               mPositionTimeRealUs %lld", playPosition, mPositionTimeRealUs);
+        mPositionTimeMediaUs = mPositionTimeRealUs;
         return mPositionTimeRealUs;
     }
 
@@ -827,6 +829,7 @@ bool AudioPlayer::getMediaTimeMapping(
         playPosition = getOutputPlayPositionUs_l();
         if(!mReachedEOS)
             mPositionTimeRealUs = playPosition;
+        mPositionTimeMediaUs = mPositionTimeRealUs;
         *realtime_us = mPositionTimeRealUs;
         *mediatime_us = mPositionTimeRealUs;
     } else {
