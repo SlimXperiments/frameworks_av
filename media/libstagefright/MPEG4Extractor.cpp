c2da32cc38d524f588ecf7bd8b50a8ea4ee8f99e
Haynes Mathew George <hgeorge@codeaurora.org>
2013-12-05 20:12:29 -0800

    libstagefright: Update handling of SBR explict siganlling
    In case of SBR explict signalling in the bitstream, the core
    AAC sample rate is overwritten by extension sample rate.
    HW AAC decoder relies on core sample rate to parse the
    bitstream when supplied in raw format.
    Do not overwrite core sample rate but store it as separate
    field in the meta data.
    
    CRs-fixed: 572242
    
    Change-Id: I5c70cfcc40a44bff9f9da4082c6b4dccd7c6788e
    
    Conflicts:
    	media/libstagefright/MPEG4Extractor.cpp

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -2525,6 +2525,10 @@ status_t MPEG4Extractor::verifyTrack(Track *track) {
 status_t MPEG4Extractor::updateAudioTrackInfoFromESDS_MPEG4Audio(
         const void *esds_data, size_t esds_size) {
     ESDS esds(esds_data, esds_size);
+    static uint32_t kSamplingRate[] = {
+        96000, 88200, 64000, 48000, 44100, 32000, 24000, 22050,
+        16000, 12000, 11025, 8000, 7350
+    };
 
     uint8_t objectTypeIndication;
     if (esds.getObjectTypeIndication(&objectTypeIndication) != OK) {
@@ -2603,7 +2607,9 @@ status_t MPEG4Extractor::updateAudioTrackInfoFromESDS_MPEG4Audio(
     uint32_t freqIndex = br.getBits(4);
 
     int32_t sampleRate = 0;
+    int32_t extSampleRate = 0;
     int32_t numChannels = 0;
+
     if (freqIndex == 15) {
         if (csd_size < 5) {
             return ERROR_MALFORMED;
@@ -2611,31 +2617,26 @@ status_t MPEG4Extractor::updateAudioTrackInfoFromESDS_MPEG4Audio(
         sampleRate = br.getBits(24);
         numChannels = br.getBits(4);
     } else {
-        numChannels = br.getBits(4);
-
         if (freqIndex == 13 || freqIndex == 14) {
             return ERROR_MALFORMED;
         }
-
+        numChannels = br.getBits(4);
         sampleRate = kSamplingRate[freqIndex];
     }
-
-    if (objectType == 5 || objectType == 29) { // SBR specific config per 14496-3 table 1.13
-        uint32_t extFreqIndex = br.getBits(4);
-        int32_t extSampleRate;
-        if (extFreqIndex == 15) {
+    if (objectType == 5 || objectType == 29) {
+        // SBR specific config per 14496-3 table 1.13
+        freqIndex = br.getBits(4);
+        if (freqIndex == 13 || freqIndex == 14) {
+            return ERROR_MALFORMED;
+        }
+        if (freqIndex == 15) {
             if (csd_size < 8) {
                 return ERROR_MALFORMED;
             }
             extSampleRate = br.getBits(24);
-        } else {
-            if (extFreqIndex == 13 || extFreqIndex == 14) {
-                return ERROR_MALFORMED;
-            }
-            extSampleRate = kSamplingRate[extFreqIndex];
         }
-        //TODO: save the extension sampling rate value in meta data =>
-        //      mLastTrack->meta->setInt32(kKeyExtSampleRate, extSampleRate);
+        extSampleRate = kSamplingRate[freqIndex];
+        mLastTrack->meta->setInt32(kKeyExtSampleRate, extSampleRate);
     }
 
     if (numChannels == 0) {
@@ -2651,6 +2652,7 @@ status_t MPEG4Extractor::updateAudioTrackInfoFromESDS_MPEG4Audio(
     }
 
     mLastTrack->meta->setInt32(kKeySampleRate, sampleRate);
+    mLastTrack->meta->setInt32(kKeyAACProfile, objectType);
 
     int32_t prevChannelCount;
     CHECK(mLastTrack->meta->findInt32(kKeyChannelCount, &prevChannelCount));
